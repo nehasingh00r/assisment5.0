9e8f4876eec8c3ebf0fbbf7881e2c7f14f6fff3b
nehasingh00r <111220329+nehasingh00r@users.noreply.github.com>
2022-09-04 05:52:02 -1200

    Add files via upload

---
+++ assisment5.0.py
@@ -0,0 +1,161 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/Designer Sneha/assisment5.0.py ===============
+enter a number5
+positive
+
+
+=============== RESTART: C:/Users/Designer Sneha/assisment5.0.py ===============
+enter a number0
+enter a number0
+SyntaxError: invalid syntax
+clear
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+
+=============== RESTART: C:/Users/Designer Sneha/assisment5.0.py ===============
+enter a number-3
+
+=============== RESTART: C:/Users/Designer Sneha/assisment5.0.py ===============
+enter a number5
+positive
+
+
+=============== RESTART: C:/Users/Designer Sneha/assisment5.0.py ===============
+enter a number-6
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number44
+number is not divisible by 5
+.py
+SyntaxError: invalid syntax
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number45
+number is divisible by 5
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number33
+number is odd
+
+========================================================= RESTART: C:/Users/Designer Sneha/assisment5.0.py ========================================================
+enter a number22
+number is even
+
+========================================================= RESTART: C:/Users/Designer Sneha/assisment5.0.py ========================================================
+enter a number10
+enter a number20
+ not greater value
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter first number10
+enter second number20
+enter second number20
+SyntaxError: invalid syntax
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter first number10
+enter second number20
+first number is not greater the second number
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter first number10
+enter second number10
+first number is not greater the second number
+
+
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the first word neha
+enter the second word priya
+ priya  neha
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the first word Bhopal
+enter the second word ajmair
+ ajmair  Bhopal
+
+========================================================= RESTART: C:/Users/Designer Sneha/assisment5.0.py ========================================================
+enter the first word bhopal
+enter the second word maha
+ maha  bhopal
+enter the first number10
+enter the second number20
+20 10
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the first wordabc
+enter the second wordbca
+bca abc
+enter the first number52
+enter the second number32
+52 32
+not a three digit number
+
+========================================================= RESTART: C:/Users/Designer Sneha/assisment5.0.py ========================================================
+enter a number333
+ three digit number
+
+========================================================= RESTART: C:/Users/Designer Sneha/assisment5.0.py ========================================================
+enter a number22
+not a three digit number
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number-2
+x is positive
+
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number2
+x is positive
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number-2
+x is negative
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter a number0
+x is zero
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2022
+leap year
+
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2022
+not leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2020
+not leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2000
+leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2024
+not leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2060
+not leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2020
+Traceback (most recent call last):
+  File "C:/Users/Designer Sneha/assisment5.0.py", line 7, in <module>
+    if year%4==0 and year%100!=0 or year%4==0:
+NameError: name 'year' is not defined
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year2020
+leap year
+
+= RESTART: C:/Users/Designer Sneha/assisment5.0.py
+enter the year 2060
+leap year
